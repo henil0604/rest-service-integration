80567358c48e43460956f9a5f1d12904fc66dbb2
henil0604 <henilmalaviya06@gmail.com>
2022-01-08 15:24:13 +0530

    Upgraded Loading Algorithm

--- src/Service.js
+++ src/Service.js
@@ -24,52 +24,59 @@ const Service = class {
     async load() {
         let model = await this.API.post("/rest-service-integration-model");
 
-        if (typeof model.data != "object") {
+        if (typeof model.data != "object" || model.data === null) {
             return false;
         }
 
         model = model.data;
+        this.model = model;
 
-        for (let key in model) {
-            const element = model[key];
-            let ElementInstance = {};
+        for (let key in this.model) {
+            const Data = this.model[key];
 
-            if (element.method.length == 1) {
+            let Instance = {};
 
-                ElementInstance = async (data) => {
-                    const response = this.API.request(element.path, {
-                        method: element.method[0],
-                        data
-                    });
-
-                    return response;
+            Instance = (data) => {
+                if (Data.method.length == 1) {
+                    return Instance[Data.method[0].toLowerCase()](data);
                 }
-                ElementInstance.__proto__.method = element.method[0];
 
-                this[key] = ElementInstance;
-
-            } else {
-                ElementInstance = {};
-
-                element.method.forEach(method => {
-
-                    ElementInstance[method.toLowerCase()] = async (data) => {
-                        const response = this.API.request(element.path, {
-                            method: method,
-                            data
-                        });
-
-                        return response;
-                    }
-
-                });
-                this[key] = ElementInstance;
-
+                if (Data.method.length > 1) {
+                    throw new Error("There are more then one HTTP Methods for this instance");
+                }
             }
+
+            Data.method.forEach(method => {
+                method = method.toLowerCase();
+
+                Instance.__proto__[method] = async (data) => {
+                    const url = Data.url;
+
+                    const response = await this.API[method](url, { data });
+
+                    return {
+                        status: response.status,
+                        data: response.data
+                    }
+                }
+
+            })
+
+
+            this[key] = Instance;
         }
 
     }
 
+    Call(name, data = {}, method = "GET") {
+        const Instance = this[name][method.toLowerCase()];
+
+        if (typeof Instance !== 'function') {
+            return null;
+        }
+
+        return Instance(data);
+    }
 
 }
 
